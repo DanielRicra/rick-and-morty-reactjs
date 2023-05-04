29f59c8895572c7a771f19922932dccf6661371a
DanielRicra <daniel.int@outlook.com>
2023-05-03 22:42:21 -0500

    refactor Card component
    - replace button symbol with trash img icon.
    - change props with destructuring character and onClose,
      change props.name,etc with character object propertipes.
    - add async function sleep to wait spesific time, and when click
      close button add sleep to add animation to our card, the execute
      onClose function with remove the current Card.

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -1,17 +1,42 @@
-import './Card.css';
+import { useRef } from 'react';
 
-function Card(props) {
+import './Card.css';
+import trashIcon from '../../assets/icons/trash.svg'
+
+function Card({ character, onClose }) {
+   const cardRef = useRef(null);
+
+   async function sleep(time) {
+      return new Promise((resolve) => setTimeout(resolve, time));
+   }
+
+   const handleClick = () => {
+      const style = cardRef?.current?.style;
+      
+      style.transition = 'all 0.3s ease-out';
+      style.transform = 'translateX(-60%) scale(0.5)';
+      style.opacity = '0';
+      style.zIndex = '0';
+      sleep(300).then(() => {
+         onClose();
+      });
+   }
 
    return (
-      <div className='card'>
-         <button type='button' className='card__close-card-button hidden' onClick={props.onClose}>✖</button>
+      <div className='card' ref={cardRef}>
+         <button type='button' className='card__close-card-button' onClick={handleClick}>
+            <img src={trashIcon} alt="delete-icon" />
+         </button>
          <div className='card__info'>
-            <h3>{props.name}</h3>
-            <p>Status: {props.status}</p>
-            <p>Specie: {props.species}</p>
-            <p>Origin: {props.origin}</p>
+            <h3>{character.name}</h3>
+            <div className='card__character-status'>
+               <span className={` ${character.status === 'Alive' ? 'green' : 'red'}`}></span>
+               <p>{character.status} - {character.species}</p>
+            </div>
+            <p><span>Origin: </span>{character.origin.name}</p>
+            <p>{character.gender}</p>
          </div>
-         <img src={props.image} alt={props.name} />
+         <img src={character.image} alt={character.name} />
       </div>
    );
 }
