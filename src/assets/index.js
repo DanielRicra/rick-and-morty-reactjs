b25b21952d6b6b67d650e7e054515e397e8ecb61
DanielRicra <daniel.int@outlook.com>
2023-05-04 21:56:08 -0500

    move icons and images in the assets folder

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+export { default as sunIcon } from './sun.svg';
+export { default as trashIcon } from './trash.svg';
+export { default as logo } from './Rick_and_Morty_logo.png'
+export { default as moonIcon } from './moon.svg';
