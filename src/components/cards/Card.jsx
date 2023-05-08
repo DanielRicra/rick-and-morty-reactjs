142530d0e5481c34f5a32110e08561957607efd2
DanielRicra <daniel.int@outlook.com>
2023-05-07 21:28:01 -0500

    refactor code and styles of Card component

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -1,7 +1,8 @@
 import { useRef } from 'react';
 
 import './Card.css';
-import trashIcon from '../../assets/icons/trash.svg'
+import { trashIcon } from '../../assets';
+import { Link } from 'react-router-dom';
 
 function Card({ character, onClose }) {
    const cardRef = useRef(null);
@@ -14,8 +15,9 @@ function Card({ character, onClose }) {
       const style = cardRef?.current?.style;
       
       style.transition = 'all 0.3s ease-out';
-      style.transform = 'translateX(-60%) scale(0.5)';
+      style.transform = 'translateX(-110%) scale(0.5)';
       style.opacity = '0';
+      style.width = '0';
       style.zIndex = '0';
       sleep(300).then(() => {
          onClose();
@@ -28,12 +30,12 @@ function Card({ character, onClose }) {
             <img src={trashIcon} alt="delete-icon" />
          </button>
          <div className='card__info'>
-            <h3>{character.name}</h3>
+            <h3><Link to={`/detail/${character.id}`}>{character.name}</Link></h3>
             <div className='card__character-status'>
                <span className={` ${character.status === 'Alive' ? 'green' : 'red'}`}></span>
                <p>{character.status} - {character.species}</p>
             </div>
-            <p><span>Origin: </span>{character.origin.name}</p>
+            <p><span>Origin: </span>{character.origin?.name}</p>
             <p>{character.gender}</p>
          </div>
          <img src={character.image} alt={character.name} />
