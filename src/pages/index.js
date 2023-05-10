6edfacde2bcafa571ec59c8a53c2ed9a941bbdda
DanielRicra <daniel.int@outlook.com>
2023-05-10 08:07:29 -0500

    add export of Favorites page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,4 @@ export { default as About } from './about/About';
 export { default as Detail } from './character-detail/Detail';
 export { default as Authentication } from './auth/Authentication';
 export { default as NotFound } from './not-found/NotFound';
+export { default as Favorites } from './favorites/Favorites'; 
