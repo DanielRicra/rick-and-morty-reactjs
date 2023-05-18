1e1da973e82bbc53c80a17b2e8d557156e3aeaee
DanielRicra <daniel.int@outlook.com>
2023-05-18 10:47:33 -0500

    Update fetchCharacterById to use a local API
    Changes
     - Changed the BASE_URL to piont to the API of the server
       in the same project.
     - Enhance the error message when throwing an error.

--- client/src/services/characterService.js
+++ client/src/services/characterService.js
@@ -1,16 +1,16 @@
-const BASE_URL = 'https://rickandmortyapi.com/api/character';
+const BASE_URL = 'http://localhost:3001/api/v1/character';
 
 export const fetchCharacterById = async (characterId) => {
    try {
       const response = await fetch(`${BASE_URL}/${characterId}`);
 
       if (!response.ok) {
-         throw new Error('Failed to fetch characters');
+         throw new Error('Character not found');
       }
 
       const data = await response.json();
       return data;
    } catch (error) {
-      throw new Error('Something went wrong');
+      throw new Error(error.message);
    }
 };
