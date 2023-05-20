f45c12809c38823620af92d7103adffeb2bfd902
DanielRicra <daniel.int@outlook.com>
2023-05-19 21:30:24 -0500

    Add index.js to export all routes

---
+++ server/src/routes/index.js
@@ -0,0 +1,3 @@
+export { default as userRoutes } from './userRoutes.js';
+export { default as characterRoutes } from './characterRoutes.js';
+export { default as favoriteRoutes } from './favoriteRoutes.js';
