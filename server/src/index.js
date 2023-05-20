2276b5b16012f72a2e46f58d02e320a4da74fee3
DanielRicra <daniel.int@outlook.com>
2023-05-20 16:57:32 -0500

    Update routes and configure CORS
    - Updated the routes to use the new index.js from routes folder
    
    - Configured CORS to allow request from any origin

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,20 @@
 import express from 'express';
-import characterRouter from './routes/characterRouter.js';
-
+import { characterRoutes, favoriteRoutes, userRoutes } from './routes/index.js';
 const PORT = 3001;
 
 const app = express();
 
-app.use('/api/v1/character', characterRouter);
+app.use(express.json());
+app.use((_, res, next) => {
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+   next();
+});
+
+app.use('/api/v1/character', characterRoutes);
+app.use('/api/v1/favorite', favoriteRoutes);
+app.use('/api/v1/user', userRoutes);
 
 app.get('/', (_, res) => {
    res.send('Welcome to the Rick and Morty API!');
