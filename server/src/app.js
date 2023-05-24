6658129ee79f7d1b9f4139aa9f42841b3e9e28b3
DanielRicra <daniel.int@outlook.com>
2023-05-24 08:56:37 -0500

    Move the express server set up to app.js
    Update index.js to import app and raise the server

---
+++ server/src/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import { characterRoutes, favoriteRoutes, userRoutes } from './routes/index.js';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use((_, res, next) => {
+   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+   next();
+});
+
+app.use('/api/v1/character', characterRoutes);
+app.use('/api/v1/favorite', favoriteRoutes);
+app.use('/api/v1/user', userRoutes);
+
+app.get('/', (_, res) => {
+   res.send('Welcome to the Rick and Morty API!');
+});
+
+export default app;
--- server/src/index.js
+++ server/src/index.js
@@ -1,30 +1,6 @@
-import express from 'express';
-import { characterRoutes, favoriteRoutes, userRoutes } from './routes/index.js';
-import morgan from 'morgan';
-
+import app from "./app.js";
 const PORT = 3001;
 
-const app = express();
-
-app.use(morgan('dev'));
-app.use(express.json());
-app.use((_, res, next) => {
-   res.header('Access-Control-Allow-Origin', '*');
-   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-   next();
-});
-
-app.use('/api/v1/character', characterRoutes);
-app.use('/api/v1/favorite', favoriteRoutes);
-app.use('/api/v1/user', userRoutes);
-
-app.get('/', (_, res) => {
-   res.send('Welcome to the Rick and Morty API!');
-});
-
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
    console.log(`server is running on port http://localhost:${PORT}`);
 });
-
-export default server;
