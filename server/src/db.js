388704131b66aee0cafe5b1669e9034ace9922b1
DanielRicra <daniel.int@outlook.com>
2023-05-31 21:43:16 -0500

    Add db.js file
    Create the sequelize instance to connect the database
    
    Added models for characters, episodes, and users by calling its functions from models folders

---
+++ server/src/db.js
@@ -0,0 +1,23 @@
+import { Sequelize } from 'sequelize';
+import createCharacterModel from './models/Character.js'
+import createEpisodeModel from './models/Episode.js';
+import createUserModel from './models/User.js';
+
+const sequelize = new Sequelize(process.env.DATABASE_URL);
+
+const Character = createCharacterModel(sequelize);
+const Episode = createEpisodeModel(sequelize);
+const User = createUserModel(sequelize);
+
+Character.belongsToMany(Episode, { through: 'character_episode' });
+Episode.belongsToMany(Character, { through: 'character_episode' });
+
+Character.belongsToMany(User, { through: 'favorite_character', timestamps: false  });
+User.belongsToMany(Character, { through: 'favorite_character', timestamps: false });
+
+export default sequelize;
+
+export { 
+   Character,
+   Episode,
+};
