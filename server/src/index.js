56349c4dd3629d3147b5abe22aabdf637a676268
DanielRicra <daniel.int@outlook.com>
2023-05-31 21:49:12 -0500

    Add the sequlize instance and sincronizing before listen the app

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,17 @@
 import app from "./app.js";
+import sequelize from './db.js';
+
 const PORT = 3001;
 
+sequelize
+   .sync({ force: false })
+   .then(() => {
+      console.log('Synced db.');
+   })
+   .catch((error) => {
+      console.log('Failed to sync db: ' + error.message);
+   });
+   
 app.listen(PORT, () => {
    console.log(`server is running on port http://localhost:${PORT}`);
 });
