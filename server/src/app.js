b0570bbd55d8d2243ff124dd5fcf3bfbe7cdb344
DanielRicra <daniel.int@outlook.com>
2023-05-31 21:48:18 -0500

    Add dotenv/config and formating the file

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { characterRoutes, favoriteRoutes, userRoutes } from './routes/index.js';
 import morgan from 'morgan';
+import 'dotenv/config.js';
 
 const app = express();
 
@@ -8,8 +9,14 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use((_, res, next) => {
    res.header('Access-Control-Allow-Origin', '*');
-   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+   res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+   );
+   res.header(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, OPTIONS'
+   );
    next();
 });
 
