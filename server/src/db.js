ea12dcb7190a3bc44a5fb63f61b4d24216acf1fe
DanielRicra <daniel.int@outlook.com>
2023-06-02 00:14:35 -0500

    Format the code

--- server/src/db.js
+++ server/src/db.js
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize';
-import defineCharacter from './models/Character.js'
+import defineCharacter from './models/Character.js';
 import defineEpisode from './models/Episode.js';
 import defineUser from './models/User.js';
 
@@ -9,15 +9,26 @@ const Character = defineCharacter(sequelize);
 const Episode = defineEpisode(sequelize);
 const User = defineUser(sequelize);
 
-Character.belongsToMany(Episode, { through: 'character_episode', timestamps: false });
-Episode.belongsToMany(Character, { through: 'character_episode', timestamps: false });
+Character.belongsToMany(Episode, {
+   through: 'character_episode',
+   timestamps: false,
+});
+Episode.belongsToMany(Character, {
+   through: 'character_episode',
+   timestamps: false,
+});
 
-Character.belongsToMany(User, { through: 'favorite_character', timestamps: false  });
-User.belongsToMany(Character, { through: 'favorite_character', timestamps: false });
+Character.belongsToMany(User, {
+   through: 'favorite_character',
+   timestamps: false,
+});
+User.belongsToMany(Character, {
+   through: 'favorite_character',
+   timestamps: false,
+});
+
+const { favorite_character: FavoriteCharacter } = sequelize.models;
 
 export default sequelize;
 
-export { 
-   Character,
-   Episode,
-};
+export { Character, Episode, User, FavoriteCharacter };
