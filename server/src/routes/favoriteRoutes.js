22adb9e38967bc1d189916d8b169dfd6732745db
DanielRicra <daniel.int@outlook.com>
2023-06-02 00:16:30 -0500

    Add route to get favorites by user id
    - Update de delete route to accept first a userId and then /characterId
      as a params

--- server/src/routes/favoriteRoutes.js
+++ server/src/routes/favoriteRoutes.js
@@ -1,10 +1,16 @@
 import { Router } from 'express';
-import { deleteFavoriteByID, getAllFavorites, saveFavorite } from '../controllers/favoriteController.js';
+import {
+   deleteFavoriteByID,
+   getAllFavorites,
+   saveFavorite,
+   getFavoritesByUserID,
+} from '../controllers/favoriteController.js';
 
 const favoriteRoutes = Router();
 
+favoriteRoutes.get('/:userId', getFavoritesByUserID);
 favoriteRoutes.get('/', getAllFavorites);
 favoriteRoutes.post('/', saveFavorite);
-favoriteRoutes.delete('/:characterID', deleteFavoriteByID);
+favoriteRoutes.delete('/:userID/:characterID', deleteFavoriteByID);
 
 export default favoriteRoutes;
